2c13cd62e878ce3199fc9b7750d62383ef261ba4
Rohan Deshpande <64836824+deshpande-rohan@users.noreply.github.com>
2021-12-04 16:00:22 +0530

    added redux-toolkit mini project

---
+++ redux-toolkit/src/components/ChangeColor.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { changeColor } from "../features/theme";
+
+const ChangeColor = () => {
+  const dispatch = useDispatch();
+  const [color, setColor] = useState("");
+
+  return (
+    <div>
+      <input type="text" onChange={(e) => setColor(e.target.value)} />
+      <button onClick={() => dispatch(changeColor(color))}>change color</button>
+    </div>
+  );
+};
+
+export default ChangeColor;
